ebf66f3518a677e934e0a2fc933d939529d2cf0c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-07-21 15:58:09 -0300

    Criado componente calculadora dentro do modulo calculadora

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalculadoraComponent
+  ],
   imports: [
     CommonModule
   ]
