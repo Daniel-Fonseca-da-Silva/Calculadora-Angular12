6bdfc106e658cf92852a7f708366900b2a89dbb3
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-07-22 01:23:20 -0300

    Adicionado método calcular em calculadora serviço

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -3,5 +3,36 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CalculadoraService {
 
+  // Constantes
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  // Método de cálculo
+  calcular(num1: number, num2: number, operacao: string): number {
+  	let resultado: number; 
+
+  	switch(operacao) {
+  	  case CalculadoraService.SOMA:
+  	    resultado = num1 + num2;
+  		break;
+  	  case CalculadoraService.SUBTRACAO:
+  	    resultado = num1 - num2;
+  		break;
+  	  case CalculadoraService.DIVISAO:
+  	    resultado = num1 / num2;
+  		break;
+  	  case CalculadoraService.MULTIPLICACAO:
+  	    resultado = num1 * num2;
+  		break;
+  	  default:
+  	    resultado = 0;
+  	}
+    
+  	return resultado;
+  }
+
 }
